cbe43ded6873d3326c66e489873906d4320a8a42
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2023-12-26 09:21:58 -0300

    Divide leap back and forward jumps in separate keymaps

--- nvim/lua/plugins/leap.lua
+++ nvim/lua/plugins/leap.lua
@@ -2,17 +2,18 @@ return {
 	"ggandor/leap.nvim",
 	config = function()
 		require("leap").setup({
-			vim.keymap.set('n', 's', function ()
-  local current_window = vim.fn.win_getid()
-  require('leap').leap { target_windows = { current_window } }
-end),
-			vim.keymap.set('n', "S", function ()
-  local focusable_windows_on_tabpage = vim.tbl_filter(
-    function (win) return vim.api.nvim_win_get_config(win).focusable end,
-    vim.api.nvim_tabpage_list_wins(0)
-  )
-  require('leap').leap { target_windows = focusable_windows_on_tabpage }
-end)
+			vim.keymap.set("n", "s", function()
+				require("leap").leap({})
+			end),
+			vim.keymap.set("n", "S", function()
+				require("leap").leap({ backward = true })
+			end),
+			vim.keymap.set("n", "<c-s>", function()
+				local focusable_windows_on_tabpage = vim.tbl_filter(function(win)
+					return vim.api.nvim_win_get_config(win).focusable
+				end, vim.api.nvim_tabpage_list_wins(0))
+				require("leap").leap({ target_windows = focusable_windows_on_tabpage })
+			end),
 		})
 	end,
 }
