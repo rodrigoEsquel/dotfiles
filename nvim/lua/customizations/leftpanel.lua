cff38a4f573719355fced78d9433128527e2b296
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-01-05 17:55:45 -0300

    Remove Sidebar plugins

--- nvim/lua/customizations/leftpanel.lua
+++ nvim/lua/customizations/leftpanel.lua
@@ -6,19 +6,8 @@ local function closeTab()
 		vim.cmd.TroubleClose()
 	elseif mode == "undo" then
 		vim.cmd.UndotreeHide()
-	elseif mode == "sidebar" then
-		require("sidebar-nvim").close()
 	else
 		vim.cmd("1wincmd c")
-	end
-end
-local function toggle_sidebar()
-	closeTab()
-	if mode == "sidebar" then
-		mode = "normal"
-	else
-		require("sidebar-nvim").open()
-		mode = "sidebar"
 	end
 end
 local function toggle_git()
@@ -50,10 +39,8 @@ local function toggle_trouble()
 	end
 end
 
-require("sidebar-nvim").setup({ initial_width = width })
 vim.g.undotree_SplitWidth = width
 
-vim.keymap.set("n", "<leader>u", toggle_undotree, {desc = "Toggle [U]ndotree sidebar"} )
-vim.keymap.set("n", "<leader>dd", toggle_trouble, {desc = "Toggle [D]iagnostics sidebar"} )
-vim.keymap.set("n", "<leader>gg", toggle_git, {desc = "Toggle [G]it sidebar"} )
-vim.keymap.set("n", "<leader>i", toggle_sidebar, {desc = "Toggle [I]nfo sidebar"})
+vim.keymap.set("n", "<leader>u", toggle_undotree, { desc = "Toggle [U]ndotree sidebar" })
+vim.keymap.set("n", "<leader>dd", toggle_trouble, { desc = "Toggle [D]iagnostics sidebar" })
+vim.keymap.set("n", "<leader>gg", toggle_git, { desc = "Toggle [G]it sidebar" })
