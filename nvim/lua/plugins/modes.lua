6393eb84f4197a5e31e7a150314b80d7d76e2650
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-01-05 11:53:35 -0300

    Lint modes file

--- nvim/lua/plugins/modes.lua
+++ nvim/lua/plugins/modes.lua
@@ -1,14 +1,13 @@
 return {
-	'mvllow/modes.nvim',
-	tag = 'v0.2.0',
+	"mvllow/modes.nvim",
+	tag = "v0.2.0",
 	config = function()
 		local theme = require("kanagawa.colors").setup({ theme = "wave" }).theme
-		require('modes').setup({
+		require("modes").setup({
 			colors = {
 				insert = theme.syn.string,
 			},
 			set_number = false,
-		}
-		)
-	end
+		})
+	end,
 }
