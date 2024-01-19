05a6280e6fa50be4b849454066a3b72250b8ca20
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-01-19 11:41:08 -0300

    Add silent property to buffer back keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -7,7 +7,7 @@ vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 vim.keymap.set("n", "<leader>bc", ":bp<bar>sp<bar>bn<bar>bd<CR>", { desc = "[C]lose [B]uffer", silent = true })
 vim.keymap.set("n", "<leader>bl", ":bnext<CR>", { desc = "Next [B]uffer" })
 vim.keymap.set("n", "<leader>bh", ":bprev<CR>", { desc = "Previous [B]uffer" })
-vim.keymap.set("n", "<leader>bb", ":b#<CR>", { desc = "[B]uffer [B]ack" })
+vim.keymap.set("n", "<leader>bb", ":b#<CR>", { desc = "[B]uffer [B]ack", silent = true })
 
 -- Remap for dealing with word wrap
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
