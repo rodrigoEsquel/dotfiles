266fe00ba3f8de2224c899ed52bc3f9a4a5cf9ef
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-01-22 09:18:06 -0300

    Add rainbow delimiters and indent lines

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,1 +1,28 @@
-return { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }
+local highlight = {
+	"@lsp.type.selfparameter",
+	"@lsp.mod.readonly",
+	"@lsp.type.namespace",
+	"@lsp.typemod.string.injected",
+	"@lsp.type.decorator",
+}
+
+return {
+	{
+		"HiPhish/rainbow-delimiters.nvim",
+		config = function()
+			vim.g.rainbow_delimiters = {
+				highlight = highlight,
+			}
+		end,
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		opts = {
+			indent = {
+				highlight = highlight,
+			},
+			-- whitespace = { highlight = highlight },
+		},
+	},
+}
