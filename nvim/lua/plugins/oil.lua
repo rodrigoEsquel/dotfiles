e204ae962919171b89f787620e949ee3e8213d7b
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-01-30 16:28:20 -0300

    Update oil.nvim config to use floating window

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -5,11 +5,27 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		require("oil").setup({
+			default_file_explorer = false,
 			view_options = {
 				show_hidden = true,
 			},
+			delete_to_trash = true,
+			lsp_rename_autosave = true,
+			float = {
+				border = "none",
+			},
+			win_options = {
+				wrap = false,
+				signcolumn = "no",
+				cursorcolumn = false,
+				foldcolumn = "0",
+				spell = false,
+				list = false,
+				conceallevel = 3,
+				concealcursor = "nvic",
+			},
 		})
-		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-		vim.keymap.set("n", "_", "<CMD>Oil .<CR>", { desc = "Open root directory" })
+		vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
+		vim.keymap.set("n", "_", "<CMD>Oil --float .<CR>", { desc = "Open root directory" })
 	end,
 }
