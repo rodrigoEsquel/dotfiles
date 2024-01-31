c4779e1924fafcf61ea891fc9c9d11d693fa403b
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-01-31 16:12:09 -0300

    Remove initial buffer

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -18,19 +18,18 @@ vim.api.nvim_create_autocmd("CursorHold", {
 	pattern = "*",
 })
 
--- local ts_group = vim.api.nvim_create_augroup("OnNvimEnter", { clear = true })
--- vim.api.nvim_create_autocmd({ "VimEnter" }, {
--- 	callback = function()
--- 		local first_arg = vim.v.argv[3]
--- 		if first_arg and vim.fn.isdirectory(first_arg) == 1 then
--- 			-- Vim creates a buffer for folder. Close it.
--- 			vim.cmd(":bd 1")
--- 			require("telescope.builtin").find_files()
--- 			-- FileExplorer  VimEnter
--- 		end
--- 	end,
--- 	group = ts_group,
--- })
+local ts_group = vim.api.nvim_create_augroup("OnNvimEnter", { clear = true })
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+	callback = function()
+		local first_arg = vim.v.argv[3]
+		if first_arg and vim.fn.isdirectory(first_arg) == 1 then
+			-- Vim creates a buffer for folder. Close it.
+			vim.cmd(":bd 1")
+			-- require("telescope.builtin").find_files()
+		end
+	end,
+	group = ts_group,
+})
 
 vim.api.nvim_exec(
 	[[
