7e1bf044b852736b39c8bbbdfe97b0c0db28b883
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-02-16 09:27:33 -0300

    Fix comment spacing

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,19 +1,19 @@
 -- See `:help vim.o`
 vim.opt.autoread = true
 vim.opt.autowrite = true -- Enable auto write
-vim.opt.confirm = true   -- Confirm to save changes before exiting
+vim.opt.confirm = true -- Confirm to save changes before exiting
 
-vim.o.hlsearch = false   -- Set highlight on search
-vim.wo.number = true     -- Make line numbers default
+vim.o.hlsearch = false -- Set highlight on search
+vim.wo.number = true -- Make line numbers default
 -- vim.opt.relativenumber = true
 
-vim.o.mouse = "a"               -- Enable mouse mode
+vim.o.mouse = "a" -- Enable mouse mode
 vim.o.clipboard = "unnamedplus" -- Sync clipboard between OS and Neovim.
-vim.o.breakindent = true        -- Enable break indent
+vim.o.breakindent = true -- Enable break indent
 
-vim.o.undofile = true           -- Save undo history
+vim.o.undofile = true -- Save undo history
 
-vim.o.ignorecase = true         -- Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
 vim.o.smartcase = true
 
 vim.wo.signcolumn = "number" -- Keep signcolumn on by default
@@ -26,19 +26,19 @@ vim.o.timeoutlen = 300
 
 vim.o.completeopt = "menu,menuone,noselect,preview" -- Set completeopt to have a better completion experience
 
-vim.o.termguicolors = true                          -- NOTE: You should make sure your terminal supports this
+vim.o.termguicolors = true -- NOTE: You should make sure your terminal supports this
 
-vim.o.scrolloff = 8                                 -- Scroll off
+vim.o.scrolloff = 8 -- Scroll off
 vim.o.sidescrolloff = 8
 
-vim.opt.colorcolumn = "80"
+-- vim.opt.colorcolumn = "80"
 
 vim.o.wrap = false -- Disable line wrap
 vim.opt.textwidth = 0
 vim.opt.wrapmargin = 0
 
 vim.opt.splitkeep = "screen" -- Keep screen placin on split
-vim.o.splitbelow = true      -- Sane split placing
+vim.o.splitbelow = true -- Sane split placing
 vim.o.splitright = true
 
 vim.opt.laststatus = 3
