887d60eb131dc6696de24266d75396da4cb329f1
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-02-16 09:24:31 -0300

    Add notification clear keybind

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -1,15 +1,19 @@
-
-	return {
-		"folke/noice.nvim",
-		event = "VeryLazy",
-		opts = {},
-		dependencies = {
-			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-			"MunifTanjim/nui.nvim",
-			-- OPTIONAL:
-			--   `nvim-notify` is only needed, if you want to use the notification view.
-			--   If not available, we use `mini` as the fallback
-			"rcarriga/nvim-notify",
-			-- vim.api.nvim_set_keymap("n", "<leader>nn", ":NoiceDismiss<CR>", {noremap=true})
-		},
-	}
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+		"MunifTanjim/nui.nvim",
+		-- OPTIONAL:
+		--   `nvim-notify` is only needed, if you want to use the notification view.
+		--   If not available, we use `mini` as the fallback
+		"rcarriga/nvim-notify",
+		vim.api.nvim_set_keymap(
+			"n",
+			"<leader>dc",
+			":NoiceDismiss<CR>",
+			{ noremap = true, desc = "Dismiss notifications" }
+		),
+	},
+	opts = {},
+}
