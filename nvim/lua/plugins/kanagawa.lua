86ef1d971d4ecc4cf04fd9adbe77e1e45126757b
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-03-08 09:35:10 -0300

    Fix kanagawa style for noice cmd popup

--- nvim/lua/plugins/kanagawa.lua
+++ nvim/lua/plugins/kanagawa.lua
@@ -1,10 +1,40 @@
 return {
 	"rebelot/kanagawa.nvim",
+	lazy = false,
 	config = function()
 		require("kanagawa").setup({
-			theme = "wave",
+			theme = "lotus",
+			colors = {
+				theme = {
+					all = {
+						ui = {
+							bg_gutter = "none",
+						},
+					},
+				},
+			},
+			overrides = function(colors)
+				local theme = colors.theme
+				return {
+					NormalFloat = { bg = "none" },
+					FloatBorder = { bg = "none" },
+					FloatTitle = { bg = "none" },
+
+					-- Save an hlgroup with dark background and dimmed foreground
+					-- so that you can use it where your still want darker windows.
+					-- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+					NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+					-- Popular plugins that open floats will link to NormalFloat by default;
+					-- set their background accordingly if you wish to keep them dark and borderless
+					LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+					MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+					-- vim.cmd("highlight NoiceCmdlinePopupBorderCmdline guibg=" .. color.bg)
+					-- vim.cmd("highlight NoiceCmdlinePopupBorderSearch guibg=" .. color.bg)
+				}
+			end,
 		})
 		vim.cmd("colorscheme kanagawa")
-		vim.cmd("highlight FloatBorder guibg=NONE")
+		-- vim.cmd("highlight FloatBorder guibg=NONE")
 	end,
 }
