a94e65ab418d490dc40cd739ebeca7fb230be012
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-03-08 09:37:59 -0300

    Clean noice config

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -8,17 +8,13 @@ return {
 		--   `nvim-notify` is only needed, if you want to use the notification view.
 		--   If not available, we use `mini` as the fallback
 		"rcarriga/nvim-notify",
-		vim.api.nvim_set_keymap(
-			"n",
+	},
+	opts = {},
+	keys = {
+		{
 			"<leader>dc",
 			":NoiceDismiss<CR>",
-			{ noremap = true, desc = "Dismiss notifications" }
-		),
+			{ noremap = true, desc = "Dismiss notifications" },
+		},
 	},
-	config = function()
-		require("noice").setup()
-		local color = require("kanagawa.colors").setup({ theme = "wave" }).theme.ui
-		vim.cmd("highlight NoiceCmdlinePopupBorderCmdline guibg=" .. color.bg)
-		vim.cmd("highlight NoiceCmdlinePopupBorderSearch guibg=" .. color.bg)
-	end,
 }
