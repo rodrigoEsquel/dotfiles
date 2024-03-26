708a67c433a2feb05f50b2e1ee32865261bd9e5b
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-03-26 08:15:51 -0300

    Add tailwind tool plugin

--- nvim/lua/plugins/tailwind.lua
+++ nvim/lua/plugins/tailwind.lua
@@ -1,24 +1,4 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        tailwindcss = {},
-      },
-    },
-  },
-  {
-    "NvChad/nvim-colorizer.lua",
-    opts = {
-      user_default_options = {
-        tailwind = true,
-      },
-    },
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
-    },
-  },
+	"luckasRanarison/tailwind-tools.nvim",
+	opts = {}, -- your configuration
 }
