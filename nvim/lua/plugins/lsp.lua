1d0731f102ca0a6312be1f24e456de3ee58d63c7
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-05-21 08:59:26 -0300

    Update signature help config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -79,7 +79,9 @@ return {
 		-- LSP settings (for overriding per client)
 		local handlers = {
 			["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
-			["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
+			["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help,
+				{ border = "rounded" }),
+
 		}
 
 		-- Enable the following language servers
