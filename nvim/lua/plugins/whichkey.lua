68299553c622e71921396704fa2a3e7a27e30470
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-11-18 06:25:29 -0300

    Update whichkey format

--- nvim/lua/plugins/whichkey.lua
+++ nvim/lua/plugins/whichkey.lua
@@ -3,20 +3,20 @@ return {
 	config = function()
 		local wk = require("which-key")
 		wk.setup()
-		wk.register({
-			["<leader>]"] = { name = "+Next" },
-			["<leader>["] = { name = "+Previous" },
-			["<leader>b"] = { name = "+[B]uffer" },
-			["<leader>c"] = { name = "+[C]ode" },
-			["<leader>d"] = { name = "+[D]iagnostic" },
-			["<leader>e"] = { name = "+[E]dgy" },
-			["<leader>g"] = { name = "+[G]it" },
-			["<leader>h"] = { name = "+[H]arpoon" },
-			["<leader>n"] = { name = "+[N]otes" },
-			["<leader>r"] = { name = "+[R]un" },
-			["<leader>s"] = { name = "+[S]earch" },
-			["<leader>t"] = { name = "+[T]est" },
-			["<leader>w"] = { name = "+[W]indow" },
+		wk.add({
+			{ "<leader>]" , group = "+Next" },
+			{ "<leader>[" , group = "+Previous" },
+			{ "<leader>b" , group = "+[B]uffer" },
+			{ "<leader>c" , group = "+[C]ode" },
+			{ "<leader>d" , group = "+[D]iagnostic" },
+			{ "<leader>e" , group = "+[E]dgy" },
+			{ "<leader>g" , group = "+[G]it" },
+			{ "<leader>h" , group = "+[H]arpoon" },
+			{ "<leader>n" , group = "+[N]otes" },
+			{ "<leader>r" , group = "+[R]un" },
+			{ "<leader>s" , group = "+[S]earch" },
+			{ "<leader>t" , group = "+[T]est" },
+			{ "<leader>w" , group = "+[W]indow" },
 		})
 	end,
 }
