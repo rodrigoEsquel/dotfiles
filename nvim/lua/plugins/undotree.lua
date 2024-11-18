2a625041ea195c83ed4eaab692198436ac1d64af
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-11-18 06:27:11 -0300

    Update untdotree config

--- nvim/lua/plugins/undotree.lua
+++ nvim/lua/plugins/undotree.lua
@@ -1,5 +1,8 @@
 return {
 	"mbbill/undotree",
+	config = function ()
+		vim.g.undotree_SetFocusWhenToggle = 1;
+	end,
 	keys = {
 		{ "<leader>u", vim.cmd.UndotreeToggle, desc = "Toggle [U]ndotree sidebar" },
 	},
