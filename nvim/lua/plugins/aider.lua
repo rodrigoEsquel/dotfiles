d136717746e288f7745f18ff24b303d7aaffe4f5
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-11-18 06:45:00 -0300

    Add aider plugin file

---
+++ nvim/lua/plugins/aider.lua
@@ -0,0 +1,9 @@
+return {
+	-- "joshuavial/aider.nvim",
+	-- opts = {
+	-- 	default_bindings = false,
+	-- },
+	-- keys = {
+	-- 	{ "<leader>a", function() vim.cmd("lua AiderOpen('--model gemini/gemini-1.5-pro-latest')") end, desc = "Run [A]ider" },
+	-- },
+}
