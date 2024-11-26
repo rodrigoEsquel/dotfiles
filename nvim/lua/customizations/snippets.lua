3bf5b2ba0c269615f1e6df1b4b1489624b9349ea
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-11-26 07:12:20 -0300

    Update snippets

--- nvim/lua/customizations/snippets.lua
+++ nvim/lua/customizations/snippets.lua
@@ -2,5 +2,8 @@ local ls = require("luasnip")
 
 ls.add_snippets("javascript", require("customizations.snippets-javascript"))
 ls.add_snippets("typescript", require("customizations.snippets-typescript"))
+ls.add_snippets("typescriptreact", require("customizations.snippets-typescript"))
 
 require("luasnip").filetype_extend("typescript", { "javascript" })
+require("luasnip").filetype_extend("javascriptreact", { "javascript" })
+require("luasnip").filetype_extend("typescriptreact", { "typescript" })
