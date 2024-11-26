9b171e18a186bbff498bc212e60a8e97cdabbd2a
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-11-26 09:23:19 -0300

    Add substitute plugin

--- nvim/lua/plugins/yanky-substitute.lua
+++ nvim/lua/plugins/yanky-substitute.lua
@@ -1,7 +1,18 @@
 return {
 	"gbprod/yanky.nvim",
+	dependencies = {
+		"gbprod/substitute.nvim",
+	},
 	config = function()
+		vim.keymap.set("n", "R", "r", { noremap = true })
+
 		require("yanky").setup({})
+
+		require("substitute").setup({
+			on_substitute = require("yanky.integration").substitute(),
+		})
+
+		vim.keymap.set("n", "r", require("substitute").operator, { noremap = true })
 		vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)")
 		vim.keymap.set({ "n", "x" }, "P", "<Plug>(YankyPutBefore)")
 		vim.keymap.set({ "n", "x" }, "gp", "<Plug>(YankyGPutAfter)")
