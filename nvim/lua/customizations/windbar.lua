3ba314e59a1e1fd9d2ae7eda78a5c7abb211d14e
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-12-04 09:00:47 -0300

    Update winbar for empty filepath

--- nvim/lua/customizations/windbar.lua
+++ nvim/lua/customizations/windbar.lua
@@ -2,6 +2,10 @@ local function filepath()
 	local file_path = vim.fn.expand("%:p")
 	local cwd = vim.fn.getcwd()
 
+	if file_path == "" then
+		return ""
+	end
+
 	-- Function to apply highlights
 	local function highlight(text, group)
 		return string.format("%%#%s#%s%%*", group, text)
@@ -45,8 +49,8 @@ local function filepath()
 
 	for i, folder in ipairs(file_folders) do
 		local group = ((i == #file_folders) and not is_oil) and "@function" or "@variable"
-
 		table.insert(folders, highlight(folder, group))
+
 		if i < #file_folders then
 			table.insert(folders, highlight(" ❯ ", "@boolean"))
 		end
