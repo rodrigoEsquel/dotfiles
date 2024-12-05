7c207fa416f9d38724f51acf1d038880711d3704
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2024-12-05 07:56:32 -0300

    Remove dashboard

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -1,15 +1,15 @@
 return {
-	"goolord/alpha-nvim",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-		"nvim-lua/plenary.nvim",
-	},
-	config = function()
-		local alpha = require("alpha")
-		local dashboard = require("alpha.themes.theta")
-		dashboard.header.val = {
-			[[ ░▒▓██▓▒░ ]],
-		}
-		alpha.setup(dashboard.config)
-	end,
+	-- "goolord/alpha-nvim",
+	-- dependencies = {
+	-- 	"nvim-tree/nvim-web-devicons",
+	-- 	"nvim-lua/plenary.nvim",
+	-- },
+	-- config = function()
+	-- 	local alpha = require("alpha")
+	-- 	local dashboard = require("alpha.themes.theta")
+	-- 	dashboard.header.val = {
+	-- 		[[ ░▒▓██▓▒░ ]],
+	-- 	}
+	-- 	alpha.setup(dashboard.config)
+	-- end,
 }
