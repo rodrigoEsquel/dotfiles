5427514ba9a19d4c5fd77b3ff73477287fcb2ec1
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2025-01-29 09:56:59 -0300

    Add command to open database

--- nvim/lua/plugins/db.lua
+++ nvim/lua/plugins/db.lua
@@ -1,4 +1,29 @@
+local function open_db_ui()
+	-- Iterate through existing tabs
+	for i = 1, vim.fn.tabpagenr("$") do
+		-- Check if the tab has a name containing "db"
+		local tabname = vim.fn.fnamemodify(vim.fn.bufname(vim.fn.tabpagebuflist(i)[1]), ":t")
+		if string.find(string.lower(tabname), "db") then
+			-- If found, switch to that tab
+			vim.cmd(i .. "tabnext")
+			return
+		end
+	end
+
+	-- If no db tab exists, create a new tab
+	vim.cmd("tabnew")
+
+	-- Optionally, set the tab name (if your Neovim setup supports tab naming)
+	vim.cmd("LualineRenameTab db")
+
+	-- Run the BUI command
+	vim.cmd("DBUI")
+end
+
+vim.keymap.set("n", "<leader>od", open_db_ui, { desc = "[O]pen [D]atabase UI" })
+
 return {
+
 	{
 		"tpope/vim-dadbod",
 		requires = {
