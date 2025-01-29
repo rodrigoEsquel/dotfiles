3ebda51fca9a9d86169c4e115b368734314cd5c4
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2025-01-29 10:16:53 -0300

    Re enable oil

--- nvim/lua/plugins/mini.files.lua
+++ nvim/lua/plugins/mini.files.lua
@@ -1,35 +1,35 @@
 return {
-	"echasnovski/mini.files",
-	dependencies = {
-		{
-			"echasnovski/mini.icons",
-			opts = {},
-		},
-	},
-	version = "*",
-	opts = {
-		mappings = {
-			go_in = "L",
-			go_in_plus = "<CR>",
-			go_out = "H",
-			go_out_plus = "-",
-			reset = "",
-		},
-		windows = {
-			width_nofocus = 35,
-			width_focus = 35,
-		},
-	},
-	keys = {
-		{ "_", "<cmd>lua MiniFiles.open()<cr>", desc = "Toggle MiniFiles" },
-		{
-			"-",
-			function()
-				local MiniFiles = require("mini.files")
-				MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
-				MiniFiles.reveal_cwd()
-			end,
-			desc = "Open MiniFiles",
-		},
-	},
+	-- "echasnovski/mini.files",
+	-- dependencies = {
+	-- 	{
+	-- 		"echasnovski/mini.icons",
+	-- 		opts = {},
+	-- 	},
+	-- },
+	-- version = "*",
+	-- opts = {
+	-- 	mappings = {
+	-- 		go_in = "L",
+	-- 		go_in_plus = "<CR>",
+	-- 		go_out = "H",
+	-- 		go_out_plus = "-",
+	-- 		reset = "",
+	-- 	},
+	-- 	windows = {
+	-- 		width_nofocus = 35,
+	-- 		width_focus = 35,
+	-- 	},
+	-- },
+	-- keys = {
+	-- 	{ "_", "<cmd>lua MiniFiles.open()<cr>", desc = "Toggle MiniFiles" },
+	-- 	{
+	-- 		"-",
+	-- 		function()
+	-- 			local MiniFiles = require("mini.files")
+	-- 			MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
+	-- 			MiniFiles.reveal_cwd()
+	-- 		end,
+	-- 		desc = "Open MiniFiles",
+	-- 	},
+	-- },
 }
