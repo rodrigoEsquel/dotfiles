2184c601ef425a178ad1014c2e91b51cb336127b
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2025-02-07 08:48:31 -0300

    Fix vimade setup

--- nvim/lua/plugins/vimade.lua
+++ nvim/lua/plugins/vimade.lua
@@ -1,11 +1,10 @@
 return {
 	"tadaa/vimade",
 	-- default opts (you can partially set these or configure them however you like)
-	opts = {
-		recipe = { "duo", { animate = false } },
-		tint = { bg = { rgb = { 0, 0, 0 }, intensity = 0.5 } },
-	},
-	keys = {
-		-- { "<leader>f", ":VimadeToggle<cr>", desc = "Toggle [F]ocus" },
-	},
+	config = function()
+		require("vimade").setup({
+			recipe = { "duo", { animate = false } },
+			tint = { bg = { rgb = { 0, 0, 0 }, intensity = 0.5 } },
+		})
+	end,
 }
