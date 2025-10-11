84ffe771f7fe244704c42dca4d3c95b4526d66c4
Rodrigo Fernandez <rodrigo.esquel@gmail.com>
2025-10-11 01:00:23 -0300

    Remove fzf dependency

--- nvim/lua/plugins/qf.lua
+++ nvim/lua/plugins/qf.lua
@@ -2,14 +2,6 @@ return {
 	"kevinhwang91/nvim-bqf",
 	ft = "qf",
 	dependencies = {
-
-		-- optional
-		{
-			"junegunn/fzf",
-			build = function()
-				vim.fn["fzf#install"]()
-			end,
-		},
 		-- optional, highly recommended
 		{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 	},
