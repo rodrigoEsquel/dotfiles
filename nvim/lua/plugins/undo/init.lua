214c410f6905a276c447b6320bb6b8e6048374c5
Rodrigo Fernandez <rodrigo@ruddr.io>
2026-04-20 10:27:16 -0300

    Install undotree

--- nvim/lua/plugins/undo/init.lua
+++ nvim/lua/plugins/undo/init.lua
@@ -1,5 +1,10 @@
 -- Native undotree (0.12+)
-vim.cmd.packadd("nvim.undotree")
+vim.api.nvim_create_autocmd("VimEnter", {
+	once = true,
+	callback = function()
+		vim.cmd.packadd("nvim.undotree")
+	end,
+})
 vim.keymap.set("n", "<leader>u", "<cmd>Undotree<CR>", { desc = "Toggle [U]ndotree sidebar" })
 
 return {}
